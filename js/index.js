593edeb5765aa6b118ded5eb0b3457e3b9929769
jose villabona <jjvillabona1@gmail.com>
2019-07-17 20:58:49 -0500

    nuevo commit

--- js/index.js
+++ js/index.js
@@ -50,3 +50,71 @@ function playVideoOnScroll(){
 
 inicializarSlider();
 playVideoOnScroll();
+
+function init(){
+  var tipos = [];
+  var ciudades = [];
+  $.get('data-1.json', function(data){
+      for(let i = 0; i < data.length; i++){
+          if(tipos.indexOf(data[i].Tipo) === -1) tipos.push(data[i].Tipo);
+          if(ciudades.indexOf(data[i].Ciudad) === -1) ciudades.push(data[i].Ciudad);
+      }
+      for(let i = 0; i < ciudades.length; i++){
+          $('#selectCiudad').append('<option value="'+ciudades[i]+'">'+ciudades[i]+'</option>');
+      }
+      for(let j = 0; j < tipos.length; j++){
+          $('#selectTipo').append('<option value="'+tipos[j]+'">'+tipos[j]+'</option>');
+      }
+      $('select').material_select();
+  });
+}
+
+$(document).ready(function(){
+  inicializarSlider();
+  init();
+});
+
+function showResult(array){
+  $('.resultados').empty();
+  for(let i=0; i<array.length; i++){
+      $('.resultados').append(`<div class="card horizontal">
+          <div class="card-image place-wrapper">
+              <img class="img-responsive place-image" src="img/${array[i].Ciudad}.jpg">
+          </div>
+          <div class="card-stacked">
+              <div class="card-content">
+                  <p>
+                      <b>Dirección: </b>${array[i].Direccion}<br>
+                      <b>Ciudad: </b>${array[i].Ciudad}<br>
+                      <b>Teléfono: </b>${array[i].Telefono}<br>
+                      <b>Código Postal: </b>${array[i].Codigo_Postal}<br>
+                      <b>Tipo: </b>${array[i].Tipo}<br>
+                      <span class="price"><b>Precio: </b>${array[i].Precio}</span>
+                  </p>
+              </div>
+              <div class="card-action">
+                  <a>Ver mas</a>
+              </div>
+          </div>
+      </div>`);
+  }
+}
+
+$('#mostrarTodos').click(function(){
+  $.get('data-1.json', function(data){
+      showResult(data);
+  });
+});
+
+$('#submitButton').click(function(){
+  let ciudad = $('#selectCiudad option:selected').val();
+  let tipo = $('#selectTipo option:selected').val();
+  let precio = $('#rangoPrecio').val();
+  console.log(ciudad + ' + ' + tipo + ' + ' + precio);
+
+  $.get('buscador.php', {ciudad:ciudad, tipo:tipo, precio:precio}, function(response){
+    let data = JSON.parse(response);
+    var r = data.data;
+    showResult(r);
+  });
+});
